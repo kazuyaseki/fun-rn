b8b495c12fd2b869dea7cb5ebec4455f091623e6
seya <kazuya.seki@bizreach.co.jp>
2018-07-26 11:58:39 +0900

    アニメーション追加

--- components/JobLists.js
+++ components/JobLists.js
@@ -1,34 +1,46 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TextInput,
-  KeyboardAvoidingView,
-  TouchableOpacity,
-  Image
-} from 'react-native';
+import { Text, Animated, Easing } from 'react-native';
 import SearchField from './SearchField';
 import styled from 'styled-components';
+import Dimensions from 'Dimensions';
 
 export default class JobLists extends React.Component {
   state = {
-    searchBarActive: false
+    searchBarActive: false,
+    jobListPosition: new Animated.Value(60)
   };
 
   openJobListField = () => {
     this.setState({ searchBarActive: false });
+
+    Animated.timing(this.state.jobListPosition, {
+      toValue: 60,
+      duration: 500,
+      easing: Easing.ease
+    }).start();
+  };
+
+  closeJobListField = () => {
+    this.setState({ searchBarActive: true });
+
+    Animated.timing(this.state.jobListPosition, {
+      toValue: Dimensions.get('window').height,
+      duration: 500,
+      easing: Easing.ease
+    }).start();
   };
 
   render() {
+    const animatedStyle = { top: this.state.jobListPosition };
+
     return (
       <JobListsWrapper>
         <SearchField
-          onFocusSearchField={() => this.setState({ searchBarActive: true })}
+          onFocusSearchField={this.closeJobListField}
           openJobListField={this.openJobListField}
         />
-        <JobList
+        <AnimatedJobList
+          style={[animatedStyle]}
           searchBarActive={this.state.searchBarActive}
           data={[{ key: 'a' }, { key: 'b' }]}
           renderItem={({ item }) => <Text>{item.key}</Text>}
@@ -44,9 +56,10 @@ const JobListsWrapper = styled.View`
 
 const JobList = styled.FlatList`
   position: absolute;
-  top: ${({ searchBarActive }) => (searchBarActive ? '100%' : 60)};
   height: 100%;
   width: 100%;
   border-radius: 16px;
   background-color: #fff;
 `;
+
+const AnimatedJobList = Animated.createAnimatedComponent(JobList);
